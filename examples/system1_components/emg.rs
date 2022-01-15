d7c3258bb27e18751f68399db993afca8f82448b
Raunak Bhagat <rabhagat31@gmail.com>
2022-01-15 14:34:08 -0800

    Add example files

--- examples/system1_components/emg.rs
+++ examples/system1_components/emg.rs
@@ -8,5 +8,45 @@ pub async fn main(txs: Txs) -> Result<(), System1Error> {
     loop {
         sleep(500u64).await;
         txs.fa_tx.send(FaMsgs::Ping).await?;
+    };
+}
+
+fn main() {
+    // loop {
+    //     let bp = check_battery();
+    //     if bp >= 20 {
+    //         check_sensor1();
+    //         check_sensor2();
+    //         check_sensor3();
+
+    //         let finger_positions = run_analytics();
+    //         update_motors(finger_positions);
+    //     }
+
+    //     else { thread::sleep(5); };
+    // };
+
+    loop {
+        let jh1 = tokio::spawn(async {
+            check_sensor1();
+            check_sensor2();
+            check_sensor3();
+            send_sensor_data();
+        });
+        let jh2 = tokio::spawn(async {
+            receive_analytics_state();
+            set_motors_and_servos();
+        });
+
+        
+
+        // check_sensor1().await;
+        // check_sensor2().await;
+        // check_sensor3().await;
+
+        // send_sensor_data().await;
+
+        // receive_analytics_state().await;
+        // set_motors_and_servos().await;
     }
 }
--- examples/system1_components/fa.rs
+++ examples/system1_components/fa.rs
@@ -1,13 +1,29 @@
 use tokio::sync::mpsc::Receiver;
 
+use crate::system1_components::System1Error;
 use crate::Txs;
 
-pub async fn main(_: Txs, mut rx: Receiver<FaMsgs>) -> Result<(), u8> {
+fn do_something() {
+    // does something
+}
+
+pub async fn main(_: Txs, mut rx: Receiver<FaMsgs>) -> Result<(), System1Error> {
     while let Some(msg) = rx.recv().await {
         match msg {
             FaMsgs::Ping => println!("got ping"),
         };
-    }
+    };
+
+    // loop {
+    //     sleep(10_000).await;
+    //     do_something();
+    //     if (rx.has_message()) {
+    //         let Some(msg) = rx.recv().await;
+    //         match msg {
+    //             FaMsgs::Ping => println!("got ping"),
+    //         };
+    //     };
+    // };
 
     Ok(())
 }
